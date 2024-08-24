a90d1d45d3f897e069b537cef4cb39981ece39a9
Yalico23 <jeanpiero_23_01@hotmail.com>
2024-08-24 16:20:19 -0500

    camio en white List

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.use(express.json())
 app.use(express.static('uploads'))
 
 //Definir un dominio(s) para recibir peticiones
-const whiteList = [process.env.FRONTED_URL]
+const whiteList = [process.env.FRONTED_URL , 'https://crm-yalico.netlify.app']
 const corsOptions = {
   origin: (origin, callback) => {
     if(!origin || whiteList.includes(origin)){
